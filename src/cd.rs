e0f6a05fc832869268b457c6667794c8e543241b
Jacob Helwig <jacob@technosorcery.net>
2016-03-24 12:21:32 -0700

    Run a shell when the cd subcommand is used
    This is hard-coded to run zsh in the current working directory for now,
    but it at least gets us running a new shell in a way that it can be
    interacted with.

--- src/cd.rs
+++ src/cd.rs
@@ -1,7 +1,19 @@
 use clap::ArgMatches;
+use std::process::Command;
 
 pub fn open_shell_at_repo(args: &ArgMatches) -> Result<(), (String, i32)> {
     println!("CD to: {}", args.value_of("repository").unwrap());
 
-    Ok(())
+    match Command::new("zsh").status() {
+        Ok(exit_status) => {
+            if exit_status.success() {
+                Ok(())
+            } else {
+                Err(("Shell failed".to_string(), exit_status.code().unwrap_or(1)))
+            }
+        },
+        Err(e) => {
+            Err((format!("{}", e), 1))
+        }
+    }
 }
